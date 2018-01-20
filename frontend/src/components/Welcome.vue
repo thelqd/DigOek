65d09081c2310027ee1e1e2b6bb176b6db40ac55
a1246760 <a1246760@unet.univie.ac.at>
2018-01-20 11:34:24 +0100

    addition at welcome page

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello col-md-8">
-    <div class="card">
+    <div class="card mb-4">
       <div class="card-header">
         <h4>{{ msg }}</h4>
       </div>
@@ -21,6 +21,35 @@ Also we are never satisfied and keep proving ourselves every day.</p>
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" id="profilecards" :src="'/static/pics/avatar.png'" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Martin Kunz</h4>
+            <p class="card-text">Business Informatics Student</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" id="profilecards" :src="'/static/pics/avatar.png'" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Daniel Schubert</h4>
+            <p class="card-text">Business Informatics Student</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" id="profilecards" :src="'/static/pics/avatar.png'" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Barbara Dimmy</h4>
+            <p class="card-text">Business Informatics Student</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,5 +66,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
+  #profilecards {
+    height: 150px;
+    width: 150px;
+    margin: 0 auto;
+    margin-top: 30px
+  }
 </style>
