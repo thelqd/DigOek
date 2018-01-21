bb4205fc8e723f4b2b90da69cb243cd80201abf5
Daniel Schubert <daniel@oldtimerz.com>
2018-01-22 00:18:08 +0100

    API create hotel

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 
 use App\Auth;
+use App\Models\Address;
 use App\Models\Hotel;
+use App\Models\HotelSupplier;
+use App\Models\Room;
 
 class ApiController extends Controller
 {
@@ -20,11 +23,54 @@ class ApiController extends Controller
      */
     public function create()
     {
-        $supplierId = $this->request->input('supplierId', 0);
-        if(Auth::checkSupplierKey($supplierId, $this->request->input('auth'))) {
-            $hotel = new Hotel();
+        $data = $this->request->input('data', null);
+        if(isset($data) && isset($data['supplierId'])) {
+            if (Auth::checkSupplierKey(
+                (int)$data['supplierId'],
+                $this->request->input('auth')
+            )) {
+                // check hotel data
+                if(isset($data['hotel']['name'])
+                    && isset($data['hotel']['description'])
+                    && is_array($data['hotel']['address'])) {
+                    // insert address
+                    $addressArray = $data['hotel']['address'];
+                    $address = new Address();
+                    $address->street = ((isset($addressArray['street'])) ? $addressArray['street'] : 'none');
+                    $address->zipcode = ((isset($addressArray['zipcode'])) ? $addressArray['zipcode'] : '0000');
+                    $address->city = ((isset($addressArray['city'])) ? $addressArray['city'] : 'none');
+                    $address->save();
+
+                    $hotel = new Hotel();
+                    $hotel->address_id = $address->id;
+                    $hotel->hotelchain_id = null;
+                    $hotel->name = $data['hotel']['name'];
+                    $hotel->description = $data['hotel']['description'];
+                    $hotel->save();
+
+                    $hotelSupplier = new HotelSupplier();
+                    $hotelSupplier->hotel_id = $hotel->id;
+                    $hotelSupplier->supplier_id = (int)$data['supplierId'];
+                    $hotelSupplier->timestamp = date('Y-m-d H:i:s');
+                    $hotelSupplier->save();
+
+                    //optional add rooms
+                    if(isset($data['hotel']['rooms']) && is_array($data['hotel']['rooms'])) {
+                        foreach($data['hotel']['rooms'] as $room) {
+                            $this->addRoom($hotel->id, $room);
+                        }
+                    }
+
+
+                    return $this->buildResponse(true, [], 'Hotel Created');
+                } else {
+                    $errorMessage = 'Hotel data incomplete';
+                }
+            } else {
+                $errorMessage = 'Api key not valid for supplier';
+            }
         } else {
-            $errorMessage = 'Api key not valid for supplier';
+            $errorMessage = 'No supplier id provided';
         }
         return $this->buildResponse(false, [], $errorMessage);
     }
@@ -38,5 +84,23 @@ class ApiController extends Controller
     {
         // delete hotel
         return $this->buildResponse(true, [], 'Hotel deleted');
+    }
+
+    /**
+     * @param integer $hotelId
+     * @param array $room
+     */
+    private function addRoom($hotelId, array $room)
+    {
+        if(array_key_exists('name', $room)
+            && array_key_exists('price', $room)
+            && array_key_exists('beds', $room)) {
+            $newRoom = new Room();
+            $newRoom->name = $room['name'];
+            $newRoom->price = $room['price'];
+            $newRoom->beds = $room['beds'];
+            $newRoom->hotel_id = $hotelId;
+            $newRoom->save();
+        }
     }
 }
